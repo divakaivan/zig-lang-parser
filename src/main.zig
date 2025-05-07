727be8f809beb925e131a80c68a88ee0433ba1c1
divakaivan <timee98@outlook.com>
2025-05-08 01:51:01 +0900

    src/main.zig i can read a file!!!

---
+++ src/main.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    const contents = try std.fs.cwd().readFileAlloc(alloc, "examples/00.lang", 4096);
+    defer alloc.free(contents);
+
+    std.debug.print("{s}", .{contents});
+}
